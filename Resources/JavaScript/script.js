d70985017f892eee1fc2ab77717a3661f49defd4
William Lamb <will333lamb@gmail.com>
2021-11-15 18:00:04 +0000

    started a question

---
+++ Resources/JavaScript/script.js
@@ -0,0 +1,7 @@
+
+let name1Array = ["Dean","Dan","Daisy","Darcey","Dominic"];
+let name1 = name1Array[Math.floor(Math.random()*5)];
+
+document.getElementById("name1").innerHTML = name1;
+
+console.log(134646);
