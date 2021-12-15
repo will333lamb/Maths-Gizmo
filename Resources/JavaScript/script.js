70b3ea6a5d0b75bd17b341d811a76bd73ca6d85d
William Lamb <will333lamb@gmail.com>
2021-12-15 16:25:43 +0000

    added new html page for displaying gcse qs

--- Resources/JavaScript/script.js
+++ Resources/JavaScript/script.js
@@ -502,6 +502,21 @@ showSolutionButton.onclick = function(){
     };
 };
 
+function generatePDF(){
+    const element = document.querySelector(".questionSolutionContainer");
+    const options = {
+        margin:       0.1,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+      };
+    html2pdf()
+    .from(element)
+    .set(options)
+    .save()
+}
+
 
 /*
 let fullscreenButton = document.getElementById("fullscreenButton");
