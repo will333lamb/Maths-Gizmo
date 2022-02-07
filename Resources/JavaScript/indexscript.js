9d06f5ab63fd572158418c7f1abac785194f3c69
William Lamb <91730466+will333lamb@users.noreply.github.com>
2022-02-07 21:44:32 +0000

    made a resize function that also reloads the page to fix chevron issue when going from portrait to landscape

--- Resources/JavaScript/indexscript.js
+++ Resources/JavaScript/indexscript.js
@@ -1,24 +1,36 @@
 
-
 const mediaQuery800px = window.matchMedia("(max-width: 800px)");
 
+let windowWidth = window.innerWidth;
 
-if(mediaQuery800px.matches){
-    document.getElementById("GCSELi").onclick = () =>{
-    if (document.getElementById("changeGCSEhtml").innerHTML === "GCSE"){
-        document.querySelector(".GCSENav").style.display = "block";
-        document.querySelector(".downChev").style.display = "none";
-        document.querySelector(".upChev").style.display = "block";
-        document.getElementById("changeGCSEhtml").innerHTML = "GCSE ";
-        
-    } else if(document.getElementById("changeGCSEhtml").innerHTML === "GCSE "){
-        document.querySelector(".GCSENav").style.display = "none";
-        document.querySelector(".downChev").style.display = "block";
-        document.querySelector(".upChev").style.display = "none";
-        document.getElementById("changeGCSEhtml").innerHTML = "GCSE"
-    }
-    }
+function resize() {
+    windowWidth = window.innerWidth;
+    console.log(windowWidth);
+    window.location.reload();
 }
+
+    
+    if(windowWidth<=800){
+        document.getElementById("GCSELi").onclick = () =>{
+        if (document.getElementById("changeGCSEhtml").innerHTML === "GCSE"){
+            document.querySelector(".GCSENav").style.display = "block";
+            document.querySelector(".downChev").style.display = "none";
+            document.querySelector(".upChev").style.display = "block";
+            document.getElementById("changeGCSEhtml").innerHTML = "GCSE ";
+            
+        } else if(document.getElementById("changeGCSEhtml").innerHTML === "GCSE "){
+            document.querySelector(".GCSENav").style.display = "none";
+            document.querySelector(".downChev").style.display = "block";
+            document.querySelector(".upChev").style.display = "none";
+            document.getElementById("changeGCSEhtml").innerHTML = "GCSE"
+        }
+        }
+    } 
+   
+   window.onresize = resize;
+   
+
+
 
 if(mediaQuery800px.matches){
 document.getElementById("FSLi").onclick = () =>{
@@ -36,3 +48,4 @@ document.getElementById("FSLi").onclick = () =>{
     }
 }
 }
+
