0793461cb4113f3dcea04593e59ea531c1548934
William Lamb <will333lamb@gmail.com>
2022-02-08 10:54:05 +0000

    fixed all resizing issues on nav bar

--- Resources/JavaScript/indexscript.js
+++ Resources/JavaScript/indexscript.js
@@ -1,19 +1,20 @@
 
-const mediaQuery800px = window.matchMedia("(max-width: 800px)");
-
-
 function getInnerWidth(){
     let windowWidth = window.innerWidth;
-    console.log(windowWidth);
 }
 
 function resize(){
     if(window.innerWidth>800){
         document.querySelector(".downChev").style.display = "none";
         document.querySelector(".upChev").style.display = "none";
-    } else if(window.innerWidth<=800 && document.querySelector(".downChev").style.display === "block"){
-        document.querySelector(".downChev").style.display = "none";
-        document.querySelector(".upChev").style.display = "block";
+        document.querySelector(".FSdownChev").style.display = "none";
+        document.querySelector(".FSupChev").style.display = "none";
+    } else if(window.innerWidth<=800 && document.querySelector(".downChev").style.display === "none"){
+        document.querySelector(".downChev").style.display = "block";
+        document.querySelector(".upChev").style.display = "none";
+    } else if(window.innerWidth<=800 && document.querySelector(".FSdownChev").style.display === "none"){
+        document.querySelector(".FSdownChev").style.display = "block";
+        document.querySelector(".FSupChev").style.display = "none";
     }
     getInnerWidth();
 }
@@ -48,21 +49,33 @@ function clickOnGCSE(){
 
 document.getElementById("GCSELi").onclick = clickOnGCSE;
 
-
-if(mediaQuery800px.matches){
-document.getElementById("FSLi").onclick = () =>{
-    if (document.getElementById("changeFShtml").innerHTML === "Functional Skills"){
+function clickOnFS(){
+    if (document.getElementById("changeFShtml").innerHTML === "Functional Skills" && window.innerWidth<=800){
         document.querySelector(".FSNav").style.display = "block";
         document.querySelector(".FSdownChev").style.display = "none";
         document.querySelector(".FSupChev").style.display = "block";
         document.getElementById("changeFShtml").innerHTML = "Functional Skills ";
-        
-    } else if(document.getElementById("changeFShtml").innerHTML === "Functional Skills "){
+    } else if(document.getElementById("changeFShtml").innerHTML === "Functional Skills " && window.innerWidth<=800){
         document.querySelector(".FSNav").style.display = "none";
         document.querySelector(".FSdownChev").style.display = "block";
         document.querySelector(".FSupChev").style.display = "none";
         document.getElementById("changeFShtml").innerHTML = "Functional Skills";
-    }
+    } else if(document.getElementById("changeFShtml").innerHTML === "Functional Skills" && window.innerWidth>800){
+        document.querySelector(".FSNav").style.display = "block";
+        document.querySelector(".FSdownChev").style.display = "none";
+        document.querySelector(".FSupChev").style.display = "none";
+        document.getElementById("changeFShtml").innerHTML = "Functional Skills ";
+        
+    } else if(document.getElementById("changeFShtml").innerHTML === "Functional Skills " && window.innerWidth>800){
+        document.querySelector(".FSNav").style.display = "none";
+        document.querySelector(".FSdownChev").style.display = "none";
+        document.querySelector(".FSupChev").style.display = "none";
+        document.getElementById("changeFShtml").innerHTML = "Functional Skills"
+    } 
 }
-}
+
+document.getElementById("FSLi").onclick = clickOnFS;
+    
+
+
 
